37fbd67c3bc14b0d7feeeeff6c3a8c83072b98c2
juansterling <2072009@maranatha.ac.id>
2022-12-11 21:38:17 +0700

    menambahkan lapangan

--- Tubes.js
+++ Tubes.js
@@ -15,3 +15,33 @@ window.addEventListener("resize", function () {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
 })
+const normal2 = new THREE.TextureLoader().load('assets/rumput_normal.jpg')
+const albedo2 = new THREE.TextureLoader().load('assets/rumput_albedo.jpg')
+
+const geometry2 = new THREE.PlaneGeometry( 30, 9 );
+const material2 = new THREE.MeshPhongMaterial( { map: albedo2 ,normalMap: normal2} );
+const plane = new THREE.Mesh( geometry2, material2 );
+plane.rotation.x = -1
+plane.position.y = -5
+scene.add( plane );
+
+
+
+const light = new THREE.PointLight();
+light.intensity = 1;
+light.position.set(-4, 0.5, 1);
+scene.add(light);
+
+const light2 = new THREE.PointLight();
+light.intensity = 1;
+light.position.set(0.5, 0.3, 2);
+scene.add(light2);
+
+// renderer.setClearColor( 0x87cefa, 1);
+function draw() {
+
+    // sphere.rotation.x +=0.05
+    renderer.render(scene, cam);
+    requestAnimationFrame(draw);
+}
+draw();
