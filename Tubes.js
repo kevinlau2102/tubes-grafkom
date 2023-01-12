5076d40c34aabcbd51f11ad23eef208fe12c1205
kevinlau2102 <kevinlaurence49@gmail.com>
2023-01-12 23:41:23 +0700

    Kick ball and delete some unnecessary files

--- Tubes.js
+++ Tubes.js
@@ -29,6 +29,40 @@ window.addEventListener("resize", function () {
 // plane.position.z = -15  
 // scene.add( plane );
 
+let rayCast = new THREE.Raycaster();
+let mouse = {};
+let selected;
+let arrow = new THREE.ArrowHelper(rayCast.ray.direction, cam.position, 300, 0xFF0000);
+scene.add(arrow);
+
+addEventListener("click", (e) => {
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = (e.clientY / window.innerHeight) * -2 + 1;
+
+    console.log(e.clientX, e.clientY);
+
+    rayCast.setFromCamera(mouse, cam);
+    let items = rayCast.intersectObjects(scene.children);
+    arrow.setDirection(rayCast.ray.direction);
+
+
+    if (((mouse.y + 0.25) * 8000) < 2100) {
+        ballBody.force.set(Math.sin(mouse.x * 2) * 5500, (mouse.y + 0.25) * 10000, (mouse.y - 1) * 7000)
+
+    } else {
+        ballBody.force.set(Math.sin(mouse.x * 2) * 5500, (mouse.y + 0.25) * 8000, (mouse.y - 1) * 7000)
+
+
+    }
+
+
+    items.forEach((i) => {
+        if (i.object.name == "defaultMaterial") {
+            console.log(i.object.name);
+            selected = i.object.name
+        }
+    });
+});
 
 
 const field = new THREE.GLTFLoader();
@@ -68,50 +102,85 @@ gawang.load('assets/gawang/scene.gltf', function (gltf) {
 
 })
 
-// const gk = new THREE.GLTFLoader();
-// gk.load('assets/gk/scene.gltf', function (gltf) {
-//     gltf.scene.scale.set(0.023, 0.023, 0.023);
-//     gltf.scene.position.y = -2.9;
-//     gltf.scene.position.z = -15;
-//     gltf.scene.position.x = 0;
+const gk = new THREE.GLTFLoader();
+let meshGoalKeeper = new THREE.Object3D();
+let mixer;
+gk.load('assets/gk/scene.gltf', function (gltf) {
+    meshGoalKeeper = gltf.scene;
+    meshGoalKeeper.scale.set(0.023, 0.023, 0.023);
+    meshGoalKeeper.position.y = -2.9;
+    meshGoalKeeper.position.z = -19;
+    meshGoalKeeper.position.x = 0;
+
+    goalKeeperBody.position.copy(meshGoalKeeper);
 
 
-//     scene.add(gltf.scene);
 
-//     const gltfAnimation = THREE.AnimationLoader
-// }, function (xhr) {
-
-//     console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
-// }, function (error) {
-
-//     console.error(error);
-
-// })
-    const loader = new THREE.FBXLoader();
-    loader.setPath('./assets/orang/');
-    loader.load('mremireh_o_desbiens.fbx', (fbx) => {
-      fbx.scale.setScalar(0.1);
-      fbx.traverse(c => {
-        c.castShadow = true;
-      });
-
-      const params = {
-        target: fbx,
-        camera: this._camera,
-      }
-    //   this._controls = new BasicCharacterControls(params);
-
-      const anim = new FBXLoader();
-      anim.setPath('./assets/orang/');
-      anim.load('walk.fbx', (anim) => {
-        const m = new THREE.AnimationMixer(fbx);
-        this._mixers.push(m);
-        const idle = m.clipAction(anim.animations[0]);
-        idle.play();
-      });
-      this._scene.add(fbx);
+    scene.add(meshGoalKeeper);
+    mixer = new THREE.AnimationMixer(meshGoalKeeper);
+    const clips = gltf.animations;
+    clips.forEach(function (clip) {
+        const action = mixer.clipAction(clip);
+        action.play();
     });
+
+}, function (xhr) {
+
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+}, function (error) {
+
+    console.error(error);
+
+})
+
+
+// const loader = new THREE.FBXLoader();
+// loader.setPath('./assets/orang/');
+// loader.load('mremireh_o_desbiens.fbx', (fbx) => {
+//   fbx.scale.setScalar(0.1);
+//   fbx.traverse(c => {
+//     c.castShadow = true;
+//   });
+
+//   const params = {
+//     target: fbx,
+//     camera: this._camera,
+//   }
+// //   this._controls = new BasicCharacterControls(params);
+
+//   const anim = new THREE.FBXLoader();
+//   anim.setPath('./assets/orang/');
+//   anim.load('walk.fbx', (anim) => {
+//     const m = new THREE.AnimationMixer(fbx);
+//     scene.add(m);
+//     const idle = m.clipAction(anim.animations[0]);
+//     idle.play();
+//   });
+//   scene.add(fbx);
+// });
+
+//     const fbxLoader = new THREE.FBXLoader();
+// fbxLoader.setPath('./assets/orang/');
+// fbxLoader.load('Ch31_nonPBR.fbx', (fbx) => {
+//   fbx.scale.setScalar(0.023);
+//   fbx.traverse(c => {
+//     c.castShadow = true;
+//   });
+
+//   scene.add(fbx);
+
+//   fbxLoader.load('./assets/orang/walk.fbx', (object) => {
+//     const animationAction = mixer.clipAction(
+//         object.animations[0]
+//     )
+//     animationActions.push(animationAction)
+//   });
+// //   this._controls = new BasicCharacterControls(params);
+
+
+// });
+
 
 
 
@@ -133,24 +202,46 @@ world.broadphase = new CANNON.NaiveBroadphase();
 let timeStamp = 1 / 60;
 
 const planeShape = new CANNON.Plane();
-const planeBody = new CANNON.Body({ shape: planeShape, mass: 0});
+const planeBody = new CANNON.Body({ shape: planeShape, mass: 0 });
 planeBody.position.set(0, -3, 0);
 planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
 world.addBody(planeBody)
 
 const gawangBack = new CANNON.Box(new CANNON.Vec3(5.7, 0.1, 2.5));
-const gawangBackBody = new CANNON.Body({shape: gawangBack, mass: 0});
+const gawangBackBody = new CANNON.Body({ shape: gawangBack, mass: 0 });
 gawangBackBody.position.set(0, -0.7, -22.2);
 gawangBackBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2.7)
 world.addBody(gawangBackBody)
 
+const gawangLeft = new CANNON.Box(new CANNON.Vec3(2.6, 0.1, 2));
+const gawangLeftBody = new CANNON.Body({ shape: gawangLeft, mass: 0 });
+gawangLeftBody.position.set(-5.7, -0.8, -21.2);
+gawangLeftBody.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 0, 1), Math.PI / 2);
+world.addBody(gawangLeftBody)
+
+const gawangRight = new CANNON.Box(new CANNON.Vec3(2.6, 0.1, 2));
+const gawangRightBody = new CANNON.Body({ shape: gawangRight, mass: 0 });
+gawangRightBody.position.set(5.8, -0.8, -21.2);
+gawangRightBody.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 0, 1), Math.PI / 2);
+world.addBody(gawangRightBody)
+
+const gawangTop = new CANNON.Box(new CANNON.Vec3(2, 0.1, 5.7));
+const gawangTopBody = new CANNON.Body({ shape: gawangTop, mass: 0 });
+gawangTopBody.position.set(0, 1.7, -21.2);
+gawangTopBody.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), Math.PI / 2);
+world.addBody(gawangTopBody)
+
+const goalKeeper = new CANNON.Box(new CANNON.Vec3(1, 3.7, 0.7));
+const goalKeeperBody = new CANNON.Body({ shape: goalKeeper, mass: 0 });
+goalKeeperBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), 0)
+world.addBody(goalKeeperBody)
 // let cannonBall = new CANNON.Sphere(1);
 // let ballBody = new CANNON.Body({shape: cannonBall, mass: 5});
 // ballBody.position.set(0, 5, 0);
 // world.addBody(ballBody);
 
 let ball = new CANNON.Sphere(0.4);
-let ballBody = new CANNON.Body({ shape: ball, mass: 5});
+let ballBody = new CANNON.Body({ shape: ball, mass: 5 });
 ballBody.position.set(0, -2, -8);
 ballBody.linearDamping = 0.31;
 world.addBody(ballBody);
@@ -159,12 +250,14 @@ world.addBody(ballBody);
 //     console.log(Math.abs(e.contact.getImpactVelocityAlongNormal()));
 // })
 
-let debugRenderer = new THREE.CannonDebugRenderer(scene, world)
+// let debugRenderer = new THREE.CannonDebugRenderer(scene, world)
 
 const soccerBall = new THREE.GLTFLoader();
 let mesh = new THREE.Object3D();
 soccerBall.load('assets/soccer_ball/scene.gltf', function (gltf) {
     mesh = gltf.scene;
+
+
 
     mesh.scale.set(0.4, 0.4, 0.4);
     mesh.position.copy(ballBody.position);
@@ -182,95 +275,132 @@ soccerBall.load('assets/soccer_ball/scene.gltf', function (gltf) {
 
 });
 
-let speed = 0;
-let acceleration = 0.002;
-let angle = 0;
-let maxSpeed = 0.5;
+// let speed = 0;
+// let acceleration = 0.002;
+// let angle = 0;
+// let maxSpeed = 0.5;
 
-let keyDown = false;
-let keyPress = "";
+// let keyDown = false;
+// let keyPress = "";
 
-document.onkeydown = (event) => {
-    if (event.key == "ArrowUp" || event.key == "ArrowDown" || event.key == "ArrowRight" || event.key == "ArrowLeft") {
-        keyDown = true;
-        keyPress = event.key;
-    }
-    // switch (event.key) {
-    //     case "ArrowUp":
-    //         speed -= acceleration;
-    //         break;
-    //     case "ArrowDown":
-    //         speed += acceleration;
-    //         break;
-    //     case "ArrowLeft":
-    //         angle += Math.PI / 180;
-    //         break;
-    //     case "ArrowRight":
-    //         angle -= Math.PI / 180;
-    //         break;
-    // }
-}
+// document.onkeydown = (event) => {
+//     if (event.key == "ArrowUp" || event.key == "ArrowDown" || event.key == "ArrowRight" || event.key == "ArrowLeft") {
+//         keyDown = true;
+//         keyPress = event.key;
+//     }
+//     // switch (event.key) {
+//     //     case "ArrowUp":
+//     //         speed -= acceleration;
+//     //         break;
+//     //     case "ArrowDown":
+//     //         speed += acceleration;
+//     //         break;
+//     //     case "ArrowLeft":
+//     //         angle += Math.PI / 180;
+//     //         break;
+//     //     case "ArrowRight":
+//     //         angle -= Math.PI / 180;
+//     //         break;
+//     // }
+// }
 
-document.onkeyup = (event) => {
-    if (event.key == "ArrowUp" || event.key == "ArrowDown" || event.key == "ArrowRight") {
-        keyDown = false;
-    }
-}
+// document.onkeyup = (event) => {
+//     if (event.key == "ArrowUp" || event.key == "ArrowDown" || event.key == "ArrowRight") {
+//         keyDown = false;
+//     }
+// }
 
-setInterval(function () {
-    if (keyDown) {
-        if (keyPress == "ArrowUp") {
-            speed -= acceleration;
-        }
-        if (keyPress == "ArrowDown") {
-            speed += acceleration;
-        }
+// setInterval(function () {
+//     if (keyDown) {
+//         if (keyPress == "ArrowUp") {
+//             speed -= acceleration;
+//         }
+//         if (keyPress == "ArrowDown") {
+//             speed += acceleration;
+//         }
 
-    }
-})
+//     }
+// })
 
-function moveBall() {
+// const search = [];
+// const lag = 0.02;
 
-    if (speed > maxSpeed) {
-        speed = maxSpeed;
-    }
-    if (speed < maxSpeed * -1) {
-        speed = -maxSpeed;
-    }
-    if (keyDown == false && speed > 0) {
-        if (speed - acceleration < 0) {
-            speed = 0
-        } else {
-            speed -= acceleration;
-        }
-    }
+// for (let i = 0; i < 360; i += 3) {
+//     search[i] = new THREE.Vector3(Math.cos(i), 0, Math.sin(i));
+// }
 
-    if (keyDown == false && speed < 0) {
-        if (speed + acceleration > 0) {
-            speed = 0
-        } else {
-            speed += acceleration;
-        }
-    }
-    
-    // ballBody.position.x += speed * Math.sin(angle);
-    // ballBody.position.z += speed * Math.cos(angle);
-    let angularVelocityX = (speed) * 180 / Math.PI;
-    ballBody.angularVelocity.set(angularVelocityX, 0, 0);
-}
+// function checkForTarget() {
+//     search.forEach((direction) => {
+//         rayCast.set(ballBody.position, direction, 0, 50);
 
 
+//         let items = rayCast.intersectObjects(scene.children, false);
+
+
+//         items.forEach((i) => {
+//             if (i.object.name) {
+//                 console.log("test");
+//                 ballBody.position.x += (direction.x * lag);
+//                 ballBody.position.z += (direction.z * lag);
+//             }
+//         });
+
+//     });
+// }
+
+
+
+// function moveBall() {
+
+//     if (speed > maxSpeed) {
+//         speed = maxSpeed;
+//     }
+//     if (speed < maxSpeed * -1) {
+//         speed = -maxSpeed;
+//     }
+//     if (keyDown == false && speed > 0) {
+//         if (speed - acceleration < 0) {
+//             speed = 0
+//         } else {
+//             speed -= acceleration;
+//         }
+//     }
+
+//     if (keyDown == false && speed < 0) {
+//         if (speed + acceleration > 0) {
+//             speed = 0
+//         } else {
+//             speed += acceleration;
+//         }
+//     }
+
+//     // ballBody.position.x += speed * Math.sin(angle);
+//     // ballBody.position.z += speed * Math.cos(angle);
+//     let angularVelocityX = (speed) * 180 / Math.PI;
+
+
+//     // if (selected == "defaultMaterial") {
+
+//     //     ballBody.force.set(0, 0, 100);
+//     // }
+// }
+
+const clock = new THREE.Clock();
 function draw() {
     world.step(timeStamp);
-    debugRenderer.update();
+    // debugRenderer.update();
     mesh.position.copy(ballBody.position);
     mesh.quaternion.copy(ballBody.quaternion);
+    goalKeeperBody.position.copy(meshGoalKeeper.position);
+    if (mixer)
+        mixer.update(clock.getDelta());
 
     // sphere.rotation.x +=0.05
     renderer.render(scene, cam);
-    controls.target.copy(ballBody.position);
-    controls.update();
-    moveBall();
+    // controls.target.copy(ballBody.position);
+    // controls.update();
+    // moveBall();
+    // checkForTarget();
     requestAnimationFrame(draw);
 }
 
